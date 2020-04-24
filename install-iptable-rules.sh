41728b3b3c4ee7f5f62e436f48065389e73f80a8
Victor Lopez <vmlopez.r@gmail.com>
2020-04-24 18:49:52 -0500

    Moved the Access Point script to "AP-install.sh", as well as the iptable rules to "install-iptable.sh". Included "disable-iptable-rules.sh" to make it easier for the user to clear them if they chose to.

--- install-iptable-rules.sh
+++ install-iptable-rules.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo iptables -A PREROUTING -t nat -i wlan0 -p tcp --dport 80 -j REDIRECT --to-destination 192.168.10.1:50000 
 sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j DNAT --to-destination 192.168.10.1:50000
 sudo iptables -t nat -A POSTROUTING -j MASQUERADE
